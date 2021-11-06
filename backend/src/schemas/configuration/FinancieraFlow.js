d60c1dd637e6a2fef8d6a1067a660d83fe5de716
Kevin Daniel Paternina Sierra <kevindps@jjk.com.co>
2021-11-06 17:01:50 -0500

    financieraFlow mongoose schema changes

--- backend/src/schemas/configuration/FinancieraFlow.js
+++ backend/src/schemas/configuration/FinancieraFlow.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-  TipoPersona: String,
+  persona: String,
   relacion: String,
   gestion: String,
   legalizacion: String,
