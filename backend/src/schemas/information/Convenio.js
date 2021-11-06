6d69e8c9bf6ff13c127a2850817e3ffc411ce6c0
Kevin Daniel Paternina Sierra <kevindps@jjk.com.co>
2021-11-05 19:50:57 -0500

    db conn changes

--- backend/src/schemas/information/Convenio.js
+++ backend/src/schemas/information/Convenio.js
@@ -20,7 +20,6 @@ const schema = mongoose.Schema({
 module.exports = mongoose
   .createConnection(process.env.MONGODB_INFORMATION_URI, {
     useNewUrlParser: true,
-    sslValidate: true,
-    sslCA: [fs.readFileSync(`${__dirname}/../rds-combined-ca-bundle.pem`)],
+    tlsCAFile: `${__dirname}/../rds-combined-ca-bundle.pem`,
   })
   .model("Convenio", schema);
