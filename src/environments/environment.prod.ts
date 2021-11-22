098278701b6d8cd0aa0d7d01ec0abe6b711a6e88
Kevin Daniel Paternina Sierra <kevindps@jjk.com.co>
2021-11-22 06:54:26 -0500

    Pre-poduction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  protocol: 'https',
-  backendAddress: 'oeiprojectflow.org',
+  //protocol: 'https',
+  //backendAddress: 'oeiprojectflow.org',
+  protocol: 'http',
+  backendAddress: '35.171.49.111:8080',
 };
