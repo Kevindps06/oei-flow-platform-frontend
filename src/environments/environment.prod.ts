4d6a3c0bcd5367e81c46d61cc512f05211c1df20
Kevin Paternina <kevindps@jjk.com.co>
2022-02-09 09:58:36 -0500

    Produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  protocol: 'http',
-  //backendAddress: 'oeiprojectflow.org',
-  //frontendAddress: 'oeiprojectflow.org',
-  backendAddress: 'lab.oeiprojectflow.org',
-  frontendAddress: 'lab.oeiprojectflow.org',
+  protocol: 'https',
+  backendAddress: 'oeiprojectflow.org',
+  frontendAddress: 'oeiprojectflow.org',
+  //backendAddress: 'lab.oeiprojectflow.org',
+  //frontendAddress: 'lab.oeiprojectflow.org',
 };
